2d4c22106468522871d642777b99189b4386008d
John Cherry <jac677@york.ac.uk>
2022-06-18 19:37:25 +0100

    Convert to classes (#3)
    * Convert server class
    
    * Convert client
    
    * Convert remaining classes
    
    * tweak options
    
    * remove single universe option

---
+++ index.d.ts
@@ -0,0 +1,5 @@
+declare module '@paradise-pi/e131';
+
+import { Client } from "./lib/client";
+import { Server } from "./lib/server";
+export { Client, Server };
--- src/client.ts
+++ src/client.ts
@@ -1,28 +1,30 @@
+import { Packet } from "./e131/packet"
+import { e131Utils } from "./utils";
 
-var dgram = require('dgram');
-var e131 = require('./utils');
+const dgram = require('dgram');
 // E1.31 client object constructor
-function Client(this: any, arg: unknown, port?: any): void {
-    if (this instanceof Client === false) {
-        return new (Client as any)(arg, port)
+export class Client {
+    private host: number|string
+    private port: number
+    private _socket: any
+
+    constructor(host: number|string, port?:number ){
+        this.host = typeof host === "number" ? e131Utils.getMulticastGroup(host) : host;
+        this.port = port || 5568;
+        this._socket = dgram.createSocket('udp4');
     }
-    if (arg === undefined) {
-        throw new TypeError('arg should be a host address, name or universe');
+
+    // create a new E1.31 packet
+    public createPacket(numSlots: number): Packet {
+        return new Packet(numSlots);
     }
-    this.host = Number.isInteger(arg) ? e131.getMulticastGroup(arg) : arg;
-    this.port = port || e131.DEFAULT_PORT;
-    this._socket = dgram.createSocket('udp4');
+
+    // send E1.31 packet
+    public send(packet: any, callback: () => {}){
+        callback = callback || function () {};
+        this._socket.send(packet.getBuffer(), this.port, this.host, function onSend() {
+            packet.incrementSequenceNumber();
+            callback();
+        });
+    }
 }
-// create a new E1.31 packet
-Client.prototype.createPacket = function createPacket(numSlots: any) {
-    return new e131.Packet(numSlots);
-};
-// send E1.31 packet
-Client.prototype.send = function send(packet: { getBuffer: () => any; incrementSequenceNumber: () => void; }, callback: () => void) {
-    callback = callback || function () { };
-    this._socket.send(packet.getBuffer(), this.port, this.host, function onSend() {
-        packet.incrementSequenceNumber();
-        callback();
-    });
-};
-export { Client };
